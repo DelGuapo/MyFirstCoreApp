79ae652df6f433d983cd7adaa6cac74f3c5d1c30
Nick <bitweaverllc@gmail.com>
2018-08-20 22:59:03 +0800

    stringify from table or from object (class)

--- MyFirstCoreApp/Assets/Stringify.cs
+++ MyFirstCoreApp/Assets/Stringify.cs
@@ -9,16 +9,16 @@ namespace MyFirstCoreApp
 {
     public class Stringify
     {
-        public string datatable(DataTable dt)
+        public string fromObject(Object obj)
         {
-            string JSONresult = JsonConvert.SerializeObject(dt);
+            string JSONresult = JsonConvert.SerializeObject(obj);
             return JSONresult;
         }
-        public IEnumerable<Dictionary<string, object>> ToDictionary(DataTable table)
+        public IEnumerable<Dictionary<string, object>> fromTable(DataTable table)
         {
             string[] columns = table.Columns.Cast<DataColumn>().Select(c => c.ColumnName).ToArray();
             IEnumerable<Dictionary<string, object>> result = table.Rows.Cast<DataRow>()
-                    .Select(dr => columns.ToDictionary(c => c, c => dr[c]));
+                    .Select(dr => columns.ToDictionary(c => c, c => (dr[c] == DBNull.Value)?null:dr[c]));
             return result;
         }
     }
