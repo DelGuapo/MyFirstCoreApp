6395eef9204835b7253afe6a177d511baa249432
Nick <bitweaverllc@gmail.com>
2018-08-20 22:57:56 +0800

    send mail async option.  adding timeout and port as inputs

--- MyFirstCoreApp/Assets/Mailify.cs
+++ MyFirstCoreApp/Assets/Mailify.cs
@@ -13,12 +13,18 @@ namespace MyFirstCoreApp
         public static string accountUser;
         public static string accountPassword;
         public static string relayServer;
-        public Mailify(string relay,string user, string password, string from)
+        public static int serverPort;
+        public static int serverTimeout;
+        public static Boolean sendEmailAsync;
+        public Mailify(string relay, string user, string password, string from, int port = 25, int timeout = 30000, Boolean sendAsync = true)
         {
             sendFrom = from;
             relayServer = relay;
             accountUser = user;
             accountPassword = password;
+            serverPort = port;
+            serverTimeout = timeout;
+            sendEmailAsync = sendAsync;
         }
         public string send(string sendTo, string subject, string body)
         {
@@ -43,18 +49,31 @@ namespace MyFirstCoreApp
 
             /* Setup Client */
             SMTPClient.Host = relayServer;
-            SMTPClient.Port = 25;
+            SMTPClient.Port = serverPort;
             SMTPClient.DeliveryMethod = SmtpDeliveryMethod.Network;
             SMTPClient.UseDefaultCredentials = true;
             SMTPClient.Credentials = new NetworkCredential(accountUser,accountPassword);
-            SMTPClient.Timeout = 30000;
+            SMTPClient.Timeout = serverTimeout;
 
 
             /* Send */
             try
             {
-                SMTPClient.Send(Message);
-                return "";
+                if (!sendEmailAsync)
+                {
+                    SMTPClient.Send(Message);
+                    return "";
+                }
+                else
+                {
+                    SMTPClient.SendCompleted += (s, e) => {
+                        SMTPClient.Dispose();
+                        SMTPClient.Dispose();
+                    };
+                    SMTPClient.SendAsync(Message, null);
+                    return "";
+                }
+                
             }
             catch (Exception err)
             {
