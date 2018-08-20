3fde2832679c7eca94115946df2c9fbff66052ca
Nick <bitweaverllc@gmail.com>
2018-08-20 22:59:23 +0800

    bluking up demo route

--- MyFirstCoreApp/Controllers/MainController.cs
+++ MyFirstCoreApp/Controllers/MainController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
+using System.Data.SqlClient;
 using System;
 using System.IO;
 using System.Text;
@@ -18,9 +19,9 @@ namespace MyFirstCoreApp.Controllers
         /// </summary>
         /// <returns>String</returns>
         [HttpGet]
-        public string Get()
+        public IActionResult Get()
         {
-            return @"                                              
+            string cool = @"                                              
                          +  .        _            .-.       
                                  3==({)_    .    (   \      
                 +  .             _  )`\-|      *  )   \     
@@ -29,15 +30,80 @@ namespace MyFirstCoreApp.Controllers
                       . *       //       (   /      .       
                                B'         `-'               
             ";
+
+            return Ok(cool);
         }
 
-        [HttpGet("test")]
-        public IActionResult Test()
+        /// <summary>
+        /// This route is for testing new functions 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("demo")]
+        public IActionResult Demo()
         {
-            string cnString = System.IO.File.ReadAllText("cnstring.txt");
-            SQLify sql = new SQLify(cnString, true);
-            DataTable rsp = sql.SqlDataTable("exec sp_who2");
-            return Ok(new Stringify().ToDictionary(rsp));
+            /* SQLify */
+            //string cnString = System.IO.File.ReadAllText("cnstring.txt");
+            //SQLify sql = new SQLify(cnString, true);
+
+            //List<SqlParameter> sqlParams = new List<SqlParameter> {
+            //    new SqlParameter("item_class", (object)"ItemCLASS"),
+            //    new SqlParameter("item_name", (object)"ItemName"),
+            //    new SqlParameter("item_value", (object)"ItemVal"),
+            //    new SqlParameter("item_descrip", (object)"ItemDescrip")
+            //};
+            //string sqlText = "exec insConfig @item_class,@item_name,@item_value,@item_descrip";
+            //DataTable rsp = sql.SqlDataTable(sqlText, sqlParams);
+
+            /* MAILify */
+            //Mailify mail = new Mailify("SMTPserver", "USER@D.COM", "PWD", "FROM@junk.com");
+            //mail.send("RECIPIENT@EMAIL.COM", "SUBJECT", "BODY");
+
+            /* Stringify */
+            //return Ok(new Stringify().fromTable(rsp));
+            //demoModel Obj = new demoModel();
+            //Obj.MyString = "TEST";
+            //Obj.MyDate = DateTime.Now;
+            //Obj.MyNumber = 1234123;
+            //Obj.MyNull = null;
+            //return Ok(new Stringify().fromObject(Obj));
+
+
+            /* Cryptify */
+            //Cryptify crypt = new Cryptify("HASH");
+            //return Ok(crypt.decode("CfDJ8PEMQfBGFHVAqO2i2UXkP1MrVyp9Rnre2X2tICeXHk4NJQ5rUgDqKBiZPLDgQyzZi4yDrRJZ3BG7vxwZZg7UgIAZ4hyG4fwdQGsN719uW6JGt1t9EWXbhjNTh3A_xO87oA"));
+
+            string rtn = @"
+             ____________________________________________________
+            /                                                    \
+           |    _____________________________________________     |
+           |   |                                             |    |
+           |   |  C:\> dotnet --info                         |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |_____________________________________________|    |
+           |                                                      |
+            \_____________________________________________________/
+                   \_______________________________________/
+                _______________________________________________
+             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
+          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
+       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
+    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
+ _-'.-.-.-.-.-. .---.-. .-----------------------------. .-.---. .---.-.-.-.`-_
+:-----------------------------------------------------------------------------:
+`---._.-----------------------------------------------------------------._.---'
+";
+
+            return Ok(rtn);
         }
     }
 }
