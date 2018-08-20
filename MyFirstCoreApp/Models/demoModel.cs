0c02aff5f2d99e4f0f3fe891c4cca39bac93aaf0
Nick <bitweaverllc@gmail.com>
2018-08-20 22:59:49 +0800

    adding demo models

---
+++ MyFirstCoreApp/Models/demoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstCoreApp
+{
+    public class demoModel
+    {
+        public string MyString { get; set; }
+        public DateTime MyDate { get; set; }
+        public int MyNumber { get; set; }
+        public string MyNull { get; set; }
+    }
+}
