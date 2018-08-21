63c5436889407cb52964c55b9904c7cb175571db
Nick <bitweaverllc@gmail.com>
2018-08-21 22:48:43 +0800

    adding http dependency to startup script.

--- MyFirstCoreApp/Startup.cs
+++ MyFirstCoreApp/Startup.cs
@@ -24,6 +24,7 @@ namespace MyFirstCoreApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddHttpClient(); // <<== REQUIRES Microsoft.Extensions.Http  (NUGET)
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
