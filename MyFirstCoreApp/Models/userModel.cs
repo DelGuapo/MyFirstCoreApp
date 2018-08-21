521ee45063ef83cfe0bfa2ff77d51a78f9e76fb8
Nick <bitweaverllc@gmail.com>
2018-08-21 22:47:53 +0800

    adding basic model for generic user.  to be used for login

---
+++ MyFirstCoreApp/Models/userModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstCoreApp
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Hash { get; set; }
+        public string Status {get;set;}
+        public string Email {get;set;}
+        public string Role {get;set;}
+        public DateTime LoginDatetime {get;set;}
+        public int LoginAttempts{get;set;}
+        public int SecondFactorAuthentication { get; set; }
+        public DateTime SecondFactorExpireDatetime { get; set; }
+        //public string SessionToken { get; set; }
+}
+}
